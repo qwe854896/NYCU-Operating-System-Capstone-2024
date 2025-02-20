9141df496b7776f1de8f7c790e6a6d691979b11a
Jun-Hong Cheng <qwe854896@gmail.com>
2025-02-21 02:26:21 +0800

    refactor: readable binary operation

--- lab1/main.zig
+++ lab1/main.zig
@@ -28,7 +28,7 @@ fn gpio_init() void {
 
     // Set GPIO 14 & 15 to ALT5 (Mini UART mode)
     var reg = GPFSEL1.read_raw();
-    reg &= ~@as(u32, 0b111111 << 12); // Clear FSEL14 and FSEL15
+    reg &= ~@as(u32, 0b111 << 12) & ~@as(u32, 0b111 << 15); // Clear FSEL14 and FSEL15
     reg |= (0b010 << 12) | (0b010 << 15); // ALT5 for UART
     GPFSEL1.write_raw(reg);
 
