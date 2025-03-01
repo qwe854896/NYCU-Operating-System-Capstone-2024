55f4e3bb75bc660cfb462ad4d03aa2e86e74b35f
Jun-Hong Cheng <qwe854896@gmail.com>
2025-03-01 15:43:12 +0800

    feat: Exception Handling

--- kernel/main.zig
+++ kernel/main.zig
@@ -25,6 +25,7 @@ const Command = enum {
     ListFiles,
     GetFileContent,
     DemoSimpleAlloc,
+    ExecFileContent,
 };
 
 fn parseCommand(command: []const u8) Command {
@@ -40,9 +41,52 @@ fn parseCommand(command: []const u8) Command {
         return Command.GetFileContent;
     } else if (std.mem.eql(u8, command, "demo")) {
         return Command.DemoSimpleAlloc;
+    } else if (std.mem.eql(u8, command, "exec")) {
+        return Command.ExecFileContent;
     } else {
         return Command.None;
     }
+}
+
+export fn exceptionEntry() void {
+    var spsr_el1: usize = undefined;
+    var elr_el1: usize = undefined;
+    var esr_el1: usize = undefined;
+
+    asm volatile (
+        \\ mrs %[arg0], spsr_el1
+        \\ mrs %[arg1], elr_el1
+        \\ mrs %[arg2], esr_el1
+        : [arg0] "=r" (spsr_el1),
+          [arg1] "=r" (elr_el1),
+          [arg2] "=r" (esr_el1),
+    );
+
+    std.log.info("Exception:", .{});
+    std.log.info("  SPSR_EL1: 0x{X}", .{spsr_el1});
+    std.log.info("  ELR_EL1: 0x{X}", .{elr_el1});
+    std.log.info("  ESR_EL1: 0x{X}", .{esr_el1});
+}
+
+fn execFile(content: []const u8) void {
+    const program_stack = simple_allocator.alloc(u8, 0x1000) catch {
+        @panic("Out of Memory! No buffer for executing a file.");
+    };
+    const program_start_address: usize = @intFromPtr(content.ptr);
+    const program_stack_address: usize = @intFromPtr(program_stack.ptr);
+    _ = mini_uart_writer.print("User program at 0x{X} will be run with the stack address 0x{X}\n", .{ program_start_address, program_stack_address }) catch {};
+    asm volatile (
+        \\ mov x1, 0x3c0
+        \\ msr spsr_el1, x1
+        \\ mov x1, %[arg0]
+        \\ msr elr_el1, x1
+        \\ mov x1, %[arg1]
+        \\ msr sp_el0, x1
+        \\ eret
+        :
+        : [arg0] "r" (program_start_address),
+          [arg1] "r" (program_stack_address),
+    );
 }
 
 fn simpleShell() void {
@@ -67,6 +111,7 @@ fn simpleShell() void {
                 _ = mini_uart_writer.write("  ls - List files in the initramfs\n") catch {};
                 _ = mini_uart_writer.write("  cat - Print the content of a file in the initramfs\n") catch {};
                 _ = mini_uart_writer.write("  demo - Run a simple allocator demo\n") catch {};
+                _ = mini_uart_writer.write("  exec - Execute a file in the initramfs\n") catch {};
             },
             Command.None => {
                 _ = mini_uart_writer.write("Unknown command: ") catch {};
@@ -111,6 +156,16 @@ fn simpleShell() void {
                 _ = mini_uart_writer.write("Buffer Content: ") catch {};
                 _ = mini_uart_writer.write(demo_buffer) catch {};
                 _ = mini_uart_writer.write("\n") catch {};
+            },
+            Command.ExecFileContent => {
+                _ = mini_uart_writer.write("Filename: ") catch {};
+                recvlen = mini_uart_reader.read(buffer) catch 0;
+                const c = cpio.getFileContent(buffer[0..recvlen]);
+                if (c) |content| {
+                    execFile(content);
+                } else {
+                    std.log.info("No such file", .{});
+                }
             },
         }
     }
@@ -169,11 +224,91 @@ comptime {
         \\ from_el2_to_el1:
         \\      mov x1, #0x00300000 // No trap to all NEON & FP instructions
         \\      msr cpacr_el1, x1   // References: https://developer.arm.com/documentation/ka006062/latest/
+        \\      adr x1, exception_vector_table
+        \\      msr vbar_el1, x1
         \\      mov x1, (1 << 31)
         \\      msr hcr_el2, x1
         \\      mov x1, 0x3c5
         \\      msr spsr_el2, x1
         \\      msr elr_el2, lr
+        \\      eret
+        \\ .align 11 // vector table should be aligned to 0x800
+        \\ .global exception_vector_table
+        \\ exception_vector_table:
+        \\      b exception_handler // branch to a handler function.
+        \\      .align 7 // entry size is 0x80, .align will pad 0
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\      b exception_handler
+        \\      .align 7
+        \\ .macro save_all
+        \\      sub sp, sp, 32 * 8
+        \\      stp x0, x1, [sp ,16 * 0]
+        \\      stp x2, x3, [sp ,16 * 1]
+        \\      stp x4, x5, [sp ,16 * 2]
+        \\      stp x6, x7, [sp ,16 * 3]
+        \\      stp x8, x9, [sp ,16 * 4]
+        \\      stp x10, x11, [sp ,16 * 5]
+        \\      stp x12, x13, [sp ,16 * 6]
+        \\      stp x14, x15, [sp ,16 * 7]
+        \\      stp x16, x17, [sp ,16 * 8]
+        \\      stp x18, x19, [sp ,16 * 9]
+        \\      stp x20, x21, [sp ,16 * 10]
+        \\      stp x22, x23, [sp ,16 * 11]
+        \\      stp x24, x25, [sp ,16 * 12]
+        \\      stp x26, x27, [sp ,16 * 13]
+        \\      stp x28, x29, [sp ,16 * 14]
+        \\      str x30, [sp, 16 * 15]
+        \\ .endm
+        \\ .macro load_all
+        \\      ldp x0, x1, [sp ,16 * 0]
+        \\      ldp x2, x3, [sp ,16 * 1]
+        \\      ldp x4, x5, [sp ,16 * 2]
+        \\      ldp x6, x7, [sp ,16 * 3]
+        \\      ldp x8, x9, [sp ,16 * 4]
+        \\      ldp x10, x11, [sp ,16 * 5]
+        \\      ldp x12, x13, [sp ,16 * 6]
+        \\      ldp x14, x15, [sp ,16 * 7]
+        \\      ldp x16, x17, [sp ,16 * 8]
+        \\      ldp x18, x19, [sp ,16 * 9]
+        \\      ldp x20, x21, [sp ,16 * 10]
+        \\      ldp x22, x23, [sp ,16 * 11]
+        \\      ldp x24, x25, [sp ,16 * 12]
+        \\      ldp x26, x27, [sp ,16 * 13]
+        \\      ldp x28, x29, [sp ,16 * 14]
+        \\      ldr x30, [sp, 16 * 15]
+        \\      add sp, sp, 32 * 8
+        \\ .endm
+        \\ exception_handler:
+        \\      save_all
+        \\      bl exceptionEntry
+        \\      load_all
         \\      eret
     );
 }
