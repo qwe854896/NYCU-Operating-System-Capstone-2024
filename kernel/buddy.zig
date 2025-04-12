6e945b29cffaf284183b77d81f4448a02b0c040d
Jun-Hong Cheng <qwe854896@gmail.com>
2025-04-12 21:49:55 +0800

    chore: add log config for buddy system

--- kernel/buddy.zig
+++ kernel/buddy.zig
@@ -7,211 +7,229 @@ const testing = std.testing;
 
 pub const Log2Usize = std.math.Log2Int(usize);
 
-pub const Buddy = struct {
-    const Self = @This();
+const Config = struct {
+    verbose_log: bool = false,
+};
 
-    /// Should be accessed from getter and setter
-    /// u5 is enough, but u8 seems to be more portable
-    log_len: u8,
-    log_longest: [1]u8,
+pub fn Buddy(comptime config: Config) type {
+    return struct {
+        const Self = @This();
 
-    /// Inference the number of pages from passed context
-    /// For example, if the ctx.len is 16, then the number of pages is 8
-    /// log_len should be 3
-    /// log_longest's size should be 15
-    pub fn init(ctx: []u8) *Self {
-        const len = ctx.len >> 1;
-        assert(isPowerOfTwo(len));
+        /// Should be accessed from getter and setter
+        /// u5 is enough, but u8 seems to be more portable
+        log_len: u8,
+        log_longest: [1]u8,
 
-        const self: *Self = @ptrCast(@alignCast(ctx));
+        /// Inference the number of pages from passed context
+        /// For example, if the ctx.len is 16, then the number of pages is 8
+        /// log_len should be 3
+        /// log_longest's size should be 15
+        pub fn init(ctx: []u8) *Self {
+            const len = ctx.len >> 1;
+            assert(isPowerOfTwo(len));
 
-        self.setLen(len);
+            const self: *Self = @ptrCast(@alignCast(ctx));
 
-        var node_size = len << 1;
+            self.setLen(len);
 
-        for (0..ctx.len - 1) |i| {
-            if (isPowerOfTwo(i + 1)) {
-                node_size >>= 1;
+            var node_size = len << 1;
+
+            for (0..ctx.len - 1) |i| {
+                if (isPowerOfTwo(i + 1)) {
+                    node_size >>= 1;
+                }
+                self.setLongest(i, node_size);
             }
-            self.setLongest(i, node_size);
+
+            if (config.verbose_log) {
+                log.info("Add page 0x{X} to order {}.", .{ 0, self.log_len });
+            }
+
+            return self;
         }
 
-        log.info("Add page 0x{X} to order {}.", .{ 0, self.log_len });
+        pub fn alloc(self: *Self, len: usize) ?usize {
+            const new_len = fixUp(len);
 
-        return self;
-    }
+            if (self.getLongest(0) < new_len) {
+                return null;
+            }
 
-    pub fn alloc(self: *Self, len: usize) ?usize {
-        const new_len = fixUp(len);
+            var index: usize = 0;
+            var node_size = self.getLen();
+            while (node_size != new_len) : (node_size >>= 1) {
+                if (config.verbose_log and self.getLongest(index) == node_size) {
+                    log.info("Remove page 0x{X} from order {}.", .{ self.indexToOffset(index), log2_int(usize, node_size) });
+                    log.info("Add page 0x{X} to order {}.", .{ self.indexToOffset(left(index)), log2_int(usize, node_size >> 1) });
+                    log.info("Add page 0x{X} to order {}.", .{ self.indexToOffset(right(index)), log2_int(usize, node_size >> 1) });
+                }
+                const left_longest = self.getLongest(left(index));
+                const right_longest = self.getLongest(right(index));
+                if (left_longest >= new_len and (right_longest < new_len or right_longest >= left_longest)) {
+                    index = left(index);
+                } else {
+                    index = right(index);
+                }
+            }
 
-        if (self.getLongest(0) < new_len) {
-            return null;
+            self.setLongest(index, 0);
+
+            const offset = (index + 1) * node_size - self.getLen();
+
+            if (config.verbose_log) {
+                log.info("Remove page 0x{X} from order {}.", .{ offset, log2_int(usize, node_size) });
+                log.info("Allocate page 0x{X} at order {}.", .{ offset, log2_int(usize, node_size) });
+            }
+
+            while (index != 0) {
+                index = parent(index);
+                self.setLongest(index, @max(self.getLongest(left(index)), self.getLongest(right(index))));
+            }
+
+            return offset;
         }
 
-        var index: usize = 0;
-        var node_size = self.getLen();
-        while (node_size != new_len) : (node_size >>= 1) {
-            if (self.getLongest(index) == node_size) {
-                log.info("Remove page 0x{X} from order {}.", .{ self.indexToOffset(index), log2_int(usize, node_size) });
-                log.info("Add page 0x{X} to order {}.", .{ self.indexToOffset(left(index)), log2_int(usize, node_size >> 1) });
-                log.info("Add page 0x{X} to order {}.", .{ self.indexToOffset(right(index)), log2_int(usize, node_size >> 1) });
+        pub fn free(self: *Self, offset: usize) void {
+            assert(offset < self.getLen());
+
+            var node_size: usize = 1;
+            var index = offset + self.getLen() - 1; // start from the leaf node
+
+            while (self.getLongest(index) != 0) : (index = parent(index)) {
+                node_size <<= 1;
+                if (index == 0) {
+                    return;
+                }
             }
-            const left_longest = self.getLongest(left(index));
-            const right_longest = self.getLongest(right(index));
-            if (left_longest >= new_len and (right_longest < new_len or right_longest >= left_longest)) {
-                index = left(index);
-            } else {
-                index = right(index);
+            self.setLongest(index, node_size);
+
+            if (config.verbose_log) {
+                log.info("Free page 0x{X} at order {}.", .{ offset, log2_int(usize, node_size) });
+                log.info("Add page 0x{X} to order {}.", .{ offset, log2_int(usize, node_size) });
+            }
+
+            while (index != 0) {
+                index = parent(index);
+                node_size <<= 1;
+
+                const left_longest = self.getLongest(left(index));
+                const right_longest = self.getLongest(right(index));
+
+                if (left_longest + right_longest == node_size) {
+                    self.setLongest(index, node_size);
+                    if (config.verbose_log) {
+                        log.info("Remove page 0x{X} from order {}.", .{ self.indexToOffset(left(index)), log2_int(usize, node_size >> 1) });
+                        log.info("Remove page 0x{X} from order {}.", .{ self.indexToOffset(right(index)), log2_int(usize, node_size >> 1) });
+                        log.info("Add page 0x{X} to order {}.", .{ self.indexToOffset(index), log2_int(usize, node_size) });
+                    }
+                } else {
+                    self.setLongest(index, @max(left_longest, right_longest));
+                }
             }
         }
 
-        self.setLongest(index, 0);
+        pub fn size(self: *const Self, offset: usize) usize {
+            return self.indexToSize(self.backward(offset));
+        }
 
-        const offset = (index + 1) * node_size - self.getLen();
+        pub fn reserve(self: *Self, start_offset: usize, end_offset: usize) void {
+            if (config.verbose_log) {
+                log.info("Reserve pages: [0x{X}, 0x{X}).", .{ start_offset, end_offset });
+            }
+            self.recursive_reserve(0, 0, self.getLen(), start_offset, end_offset);
+        }
 
-        log.info("Remove page 0x{X} from order {}.", .{ offset, log2_int(usize, node_size) });
-        log.info("Allocate page 0x{X} at order {}.", .{ offset, log2_int(usize, node_size) });
+        fn recursive_reserve(self: *Self, index: usize, left_offset: usize, right_offset: usize, start_offset: usize, end_offset: usize) void {
+            if (right_offset <= start_offset or end_offset <= left_offset) {
+                return;
+            }
+            if (start_offset <= left_offset and right_offset <= end_offset) {
+                self.setLongest(index, 0);
+                if (config.verbose_log) {
+                    log.info("Remove page 0x{X} from order {}.", .{ self.indexToOffset(index), log2_int(usize, right_offset - left_offset) });
+                }
+                return;
+            }
 
-        while (index != 0) {
-            index = parent(index);
+            if (config.verbose_log and self.getLongest(index) == right_offset - left_offset) {
+                log.info("Remove page 0x{X} from order {}.", .{ self.indexToOffset(index), log2_int(usize, right_offset - left_offset) });
+                log.info("Add page 0x{X} to order {}.", .{ self.indexToOffset(left(index)), log2_int(usize, (right_offset - left_offset) >> 1) });
+                log.info("Add page 0x{X} to order {}.", .{ self.indexToOffset(right(index)), log2_int(usize, (right_offset - left_offset) >> 1) });
+            }
+
+            const middle_offset = (left_offset + right_offset) >> 1;
+            self.recursive_reserve(left(index), left_offset, middle_offset, start_offset, end_offset);
+            self.recursive_reserve(right(index), middle_offset, right_offset, start_offset, end_offset);
+
             self.setLongest(index, @max(self.getLongest(left(index)), self.getLongest(right(index))));
         }
 
-        return offset;
-    }
-
-    pub fn free(self: *Self, offset: usize) void {
-        assert(offset < self.getLen());
-
-        var node_size: usize = 1;
-        var index = offset + self.getLen() - 1; // start from the leaf node
-
-        while (self.getLongest(index) != 0) : (index = parent(index)) {
-            node_size <<= 1;
-            if (index == 0) {
-                return;
+        fn backward(self: *const Self, offset: usize) usize {
+            assert(offset < self.getLen());
+            var index = offset + self.getLen() - 1; // start from leaf node
+            while (self.getLongest(index) != 0) {
+                index = parent(index);
             }
+            return index;
         }
-        self.setLongest(index, node_size);
 
-        log.info("Free page 0x{X} at order {}.", .{ offset, log2_int(usize, node_size) });
-        log.info("Add page 0x{X} to order {}.", .{ offset, log2_int(usize, node_size) });
+        inline fn setLen(self: *Self, len: usize) void {
+            self.log_len = log2_int(usize, len);
+        }
 
-        while (index != 0) {
-            index = parent(index);
-            node_size <<= 1;
+        inline fn getLen(self: *const Self) usize {
+            return @as(usize, 1) << @as(Log2Usize, @intCast(self.log_len));
+        }
 
-            const left_longest = self.getLongest(left(index));
-            const right_longest = self.getLongest(right(index));
+        /// Map node_size
+        /// 0 1 2 4 8 16 32 ...
+        /// to log_longest
+        /// 0 1 2 3 4 5  6  ...
+        inline fn setLongest(self: *Self, index: usize, node_size: usize) void {
+            const ptr: [*]u8 = @ptrCast(&self.log_longest);
+            ptr[index] = log2_int(usize, node_size << 1 | 1);
+        }
 
-            if (left_longest + right_longest == node_size) {
-                self.setLongest(index, node_size);
-                log.info("Remove page 0x{X} from order {}.", .{ self.indexToOffset(left(index)), log2_int(usize, node_size >> 1) });
-                log.info("Remove page 0x{X} from order {}.", .{ self.indexToOffset(right(index)), log2_int(usize, node_size >> 1) });
-                log.info("Add page 0x{X} to order {}.", .{ self.indexToOffset(index), log2_int(usize, node_size) });
-            } else {
-                self.setLongest(index, @max(left_longest, right_longest));
+        inline fn getLongest(self: *const Self, index: usize) usize {
+            const ptr: [*]const u8 = @ptrCast(&self.log_longest);
+            return (@as(usize, 1) << @as(Log2Usize, @intCast(ptr[index]))) >> 1;
+        }
+
+        fn fixUp(len: usize) usize {
+            if (len == 0) {
+                return 1;
             }
-        }
-    }
-
-    pub fn size(self: *const Self, offset: usize) usize {
-        return self.indexToSize(self.backward(offset));
-    }
-
-    pub fn reserve(self: *Self, start_offset: usize, end_offset: usize) void {
-        log.info("Reserve pages: [0x{X}, 0x{X}).", .{ start_offset, end_offset });
-        self.recursive_reserve(0, 0, self.getLen(), start_offset, end_offset);
-    }
-
-    fn recursive_reserve(self: *Self, index: usize, left_offset: usize, right_offset: usize, start_offset: usize, end_offset: usize) void {
-        if (right_offset <= start_offset or end_offset <= left_offset) {
-            return;
-        }
-        if (start_offset <= left_offset and right_offset <= end_offset) {
-            self.setLongest(index, 0);
-            log.info("Remove page 0x{X} from order {}.", .{ self.indexToOffset(index), log2_int(usize, right_offset - left_offset) });
-            return;
+            var n = len - 1;
+            n |= n >> 1;
+            n |= n >> 2;
+            n |= n >> 4;
+            n |= n >> 8;
+            n |= n >> 16;
+            n |= n >> 32;
+            return n + 1;
         }
 
-        if (self.getLongest(index) == right_offset - left_offset) {
-            log.info("Remove page 0x{X} from order {}.", .{ self.indexToOffset(index), log2_int(usize, right_offset - left_offset) });
-            log.info("Add page 0x{X} to order {}.", .{ self.indexToOffset(left(index)), log2_int(usize, (right_offset - left_offset) >> 1) });
-            log.info("Add page 0x{X} to order {}.", .{ self.indexToOffset(right(index)), log2_int(usize, (right_offset - left_offset) >> 1) });
+        inline fn left(index: usize) usize {
+            return index << 1 | 1;
         }
 
-        const middle_offset = (left_offset + right_offset) >> 1;
-        self.recursive_reserve(left(index), left_offset, middle_offset, start_offset, end_offset);
-        self.recursive_reserve(right(index), middle_offset, right_offset, start_offset, end_offset);
-
-        self.setLongest(index, @max(self.getLongest(left(index)), self.getLongest(right(index))));
-    }
-
-    fn backward(self: *const Self, offset: usize) usize {
-        assert(offset < self.getLen());
-        var index = offset + self.getLen() - 1; // start from leaf node
-        while (self.getLongest(index) != 0) {
-            index = parent(index);
+        inline fn right(index: usize) usize {
+            return (index << 1) + 2;
         }
-        return index;
-    }
 
-    inline fn setLen(self: *Self, len: usize) void {
-        self.log_len = log2_int(usize, len);
-    }
-
-    inline fn getLen(self: *const Self) usize {
-        return @as(usize, 1) << @as(Log2Usize, @intCast(self.log_len));
-    }
-
-    /// Map node_size
-    /// 0 1 2 4 8 16 32 ...
-    /// to log_longest
-    /// 0 1 2 3 4 5  6  ...
-    inline fn setLongest(self: *Self, index: usize, node_size: usize) void {
-        const ptr: [*]u8 = @ptrCast(&self.log_longest);
-        ptr[index] = log2_int(usize, node_size << 1 | 1);
-    }
-
-    inline fn getLongest(self: *const Self, index: usize) usize {
-        const ptr: [*]const u8 = @ptrCast(&self.log_longest);
-        return (@as(usize, 1) << @as(Log2Usize, @intCast(ptr[index]))) >> 1;
-    }
-
-    fn fixUp(len: usize) usize {
-        if (len == 0) {
-            return 1;
+        inline fn parent(index: usize) usize {
+            return ((index + 1) >> 1) - 1;
         }
-        var n = len - 1;
-        n |= n >> 1;
-        n |= n >> 2;
-        n |= n >> 4;
-        n |= n >> 8;
-        n |= n >> 16;
-        n |= n >> 32;
-        return n + 1;
-    }
 
-    inline fn left(index: usize) usize {
-        return index << 1 | 1;
-    }
+        inline fn indexToSize(self: *const Self, index: usize) usize {
+            return self.getLen() >> log2_int(usize, index + 1);
+        }
 
-    inline fn right(index: usize) usize {
-        return (index << 1) + 2;
-    }
-
-    inline fn parent(index: usize) usize {
-        return ((index + 1) >> 1) - 1;
-    }
-
-    inline fn indexToSize(self: *const Self, index: usize) usize {
-        return self.getLen() >> log2_int(usize, index + 1);
-    }
-
-    inline fn indexToOffset(self: *const Self, index: usize) usize {
-        return (index + 1) * self.indexToSize(index) - self.getLen();
-    }
-};
+        inline fn indexToOffset(self: *const Self, index: usize) usize {
+            return (index + 1) * self.indexToSize(index) - self.getLen();
+        }
+    };
+}
 
 test "Buddy" {
     const heap_size = 16;
@@ -220,7 +238,7 @@ test "Buddy" {
         var ctx: [2 * heap_size]u8 = undefined;
     };
 
-    var buddy = Buddy.init(S.ctx[0..]);
+    var buddy = Buddy(.{}).init(S.ctx[0..]);
 
     for (0..16) |i| {
         const offset = buddy.alloc(1).?;
